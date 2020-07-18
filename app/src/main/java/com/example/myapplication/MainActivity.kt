334231b7e6195c89d192f460138ae67443d3c01a
yoruhi-net <yoruhi@yoruhi.net>
2020-07-18 15:07:32 +0900

    ver3

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnOnClick(view: View){
+        textView1.text = getString(R.string.txt_after)
+    }
+}
